162d9b0faa316cb7c6d77934430135bb53d96975
hjxhzml <1726283687@qq.com>
2019-01-05 15:22:18 +0800

    FloatDemo(Folat的练习)

---
+++ src/test/FloatDemo.java
@@ -0,0 +1,24 @@
+package test;
+
+public class FloatDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 定义一个float类型的变量f1，值为98.4
+	    float f1 = 98.4f;
+		//定义一个float类型的变量f2，将f1的值赋值给f2
+		float f2 = 98.4f;
+		//定义一个整型变量n，值为55
+		int n = 55;
+		//定义一个double类型的变量d1，值为555.3
+		double d1 = 555.3;
+		System.out.println(d1);
+		//将n的值赋值给d1
+		d1 = n;
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println(n);
+		System.out.println(d1);
+	}
+
+}
